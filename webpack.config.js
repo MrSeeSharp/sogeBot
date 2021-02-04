d82ef69d9a1e1714ddb93aaf3755d553f225fdd8
soge__ <sogehige@gmail.com>
2021-02-04 22:16:04 +0100

    fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -108,11 +108,6 @@ const webpackConfig = {
           'css-loader',
           {
             loader: 'sass-loader',
-            // Requires sass-loader@^7.0.0
-            options: {
-              implementation: require('sass'),
-              indentedSyntax: true // optional
-            },
             // Requires sass-loader@^8.0.0
             options: {
               implementation: require('sass'),
